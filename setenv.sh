962568f509a4c54a47fa8152b5d129dfa419be4f
Kim, Jeongnim <jeongnim.kim@intel.com>
2020-12-14 10:27:58 -0800

    Update with gold

--- setenv.sh
+++ setenv.sh
@@ -5,12 +5,17 @@ alias h=history
 alias c=clear
 alias rm='rm -i'
 
+alias sth1='export OMP_NUM_THREADS=1'
+alias sth2='export OMP_NUM_THREADS=2'
+alias sth4='export OMP_NUM_THREADS=4'
+
+
 #source /opt/intel/inteloneapi/setvars.sh
 export PATH="/workspace/qmstk/common/bin:$PATH"
-export CPATH="/workspace/qmstk/common/include:$CPATH"
+export CPATH="/workspace/qmstk/common/boost:/workspace/qmstk/common/include:$CPATH"
 export LIBRARY_PATH="/workspace/qmstk/common/lib:$LIBRARY_PATH"
 export LD_LIBRARY_PATH="/workspace/qmstk/common/lib:$LD_LIBRARY_PATH"
 
-#use host OpenMP with Sycl
-export CPATH="/opt/intel/inteloneapi/compiler/latest/linux/compiler/include:$CPATH"
-#export LIBRARY_PATH="/opt/intel/inteloneapi/compiler/2021.1-alpha01/lib/oclfpga/llvm/lib:$LIBRARY_PATH"
+echo "docker on macOS"
+echo "export SYCL_DEVICE_TYPE=CPU"
+echo "export OMP_TARGET_OFFLOAD=DISABLED|MANDATORY|DEFAULT"
