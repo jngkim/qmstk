cc950ceb7b66aaa0754c892c5d4a77907641de16
Kim, Jeongnim <jeongnim.kim@intel.com>
2020-12-23 12:00:23 -0800

    Add cmake build using external packages.

---
+++ qmstk-cmake/ext_tests/xml_read.cpp
@@ -0,0 +1,18 @@
+#include <libxml/parser.h>
+#include <string>
+#include <iostream>
+
+
+int main()
+{
+
+  std::string s("<?xml version=\"1.0\"?> \
+      <project> \
+      </project> ");
+
+  xmlDocPtr m_doc = xmlParseMemory(s.data(), s.size());
+  xmlFreeDoc(m_doc);
+
+  return 0;
+}
+
