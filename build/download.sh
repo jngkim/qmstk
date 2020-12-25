c28f8d1f413daac4fe47df95dbc8b0b1d9a1c8a2
u56496 <u56496@login-2.aidevcloud>
2020-12-25 09:48:43 -0800

    Update

--- build/download.sh
+++ build/download.sh
@@ -1,26 +1,40 @@
 #!/bin/bash
 
+dest=Source
+
 ###download hdf5
-mkdir -p Source
-if [ ! -x "hdf5" ]; then
+mkdir -p $dest
+
+if [ ! -x "$dest/hdf5" ]; then
   v=1.10.5
-  wget https://support.hdfgroup.org/ftp/HDF5/current/src/hdf5-${v}.tar.gz
-  tar -zxf hdf5-${v}.tar.gz  
-  mv hdf5-${v} Source/hdf5
+  file=hdf5-${v}.tar.gz 
+  if [ ! -f "$file" ]; then
+    wget https://support.hdfgroup.org/ftp/HDF5/current/src/$file
+  fi
+  tar -zxf $file 
+  mv hdf5-${v} $dest/hdf5
 fi
-#
-#download cmake 
-if [ ! -x "cmake" ]; then
-  v=3.19.1
-  wget https://github.com/Kitware/CMake/releases/download/v${v}/cmake-${v}.tar.gz
-  tar -zxf cmake-${v}.tar.gz 
+
+##
+##download cmake 
+if [ ! -x "$dest/cmake" ]; then
+  v=3.19.2
+  file=cmake-${v}.tar.gz
+  if [ ! -f "$file" ]; then
+    wget https://github.com/Kitware/CMake/releases/download/v${v}/$file
+  fi
+  tar -zxf $file 
   mv cmake-${v} Source/cmake
 fi
 
-##dowlonad libxml2
-if [ ! -x "libxml2" ]; then
-  wget  http://xmlsoft.org/sources/libxml2-2.9.8.tar.gz
-  tar -zxf libxml2-2.9.8.tar.gz 
+###dowlonad libxml2
+if [ ! -x "$dest/libxml2" ]; then
+  v=2.9.8
+  file=libxml2-${v}.tar.gz
+  if [ ! -f "$file" ]; then
+    wget  http://xmlsoft.org/sources/$file
+  fi
+  tar -zxf $file 
   mv libxml2-2.9.8 Source/libxml2
 fi
 
